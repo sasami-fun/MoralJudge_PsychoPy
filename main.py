e58ecdf72ea27e5adc4be4219b519ebb9cb9e598
@sasami-fun <hi.sasamori.n09@gmail.com>
2021-11-02 11:40:58 +0900

    作為不作為人身的非人身的刺激提示、提示時間と反応時間の記録

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from psychopy import visual, core, event, gui
 import random
+import csv
 import pathlib
 import datetime
+#import pandas as pd
 
 subj_info = {"参加者ID":'', "年齢":'', "性別": ["男性", "女性", "その他", "未回答"]}
 
@@ -15,41 +17,59 @@ subjID = subj_info["参加者ID"]
 age = subj_info["年齢"]
 gender = subj_info["性別"]
 
+sentence = []
+with open('stims_v2.csv','r', encoding = 'utf-8-sig') as f:
+    reader = csv.reader(f)
+    header = reader.__next__()
+
+with open('stims_v2.csv','r', encoding = 'utf-8-sig') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        sentence.append(row)
+        sentence.append(row)
 win = visual.Window()
 
-letter_stim = visual.ImageStim(win) 
-letter_pos = [["stim1.png",0]]
+sentence_stim = visual.TextStim(win) 
+random.shuffle(sentence)
 
 
 current_folder = pathlib.Path(__file__).parent
-new_filename = "{}_{}.csv".format(subjID,datetime.date.today()) 
+new_filename = "{}_{}_{}_{}.csv".format(subjID,age,gender,subjID,datetime.date.today())
 new_filepath = current_folder/"data"/new_filename
 
 datafile = open(new_filepath, mode = 'a')
-datafile.write('ID, age, gender, trialID, stim, pos, respKey, respTime\n')
-
-trialID = 0
+datafile.write('TimeStamp, Status/Key\n')
 
 stopwatch = core.Clock()
+timeStamped = stopwatch
 
-for letter, x_axis in letter_pos:
-    photo = visual.ImageStim(win,"stim1.png")
-    letter_stim.setPos([x_axis, 0])
-    photo.draw()
-    win.flip()
+for line in range(0,52):
+    for column in range(1, 7):
+        sentence_stim = visual.TextStim(win, sentence[line][column])
+        sentence_stim.draw()
+        win.flip()
+        data = '{},{}-{}\n'.format(stopwatch.getTime(),sentence[line][0],header[column] )
+        datafile.write(data)
+        
+        if column > 5:
+            resp = event.waitKeys(keyList = ['right','left','escape'], timeStamped = stopwatch)
+            key, rt = resp[0]
+            if key == 'escape': 
+                datafile.close() 
+                core.quit()
+            data = '{},{}\n'.format(rt, key)
+            datafile.write(data)
 
-    stopwatch.reset()
-    resp = event.waitKeys(keyList = ['left', 'right', 'escape'], timeStamped = stopwatch)
-
-    key, rt = resp[0]
-    if key == 'escape': 
-        datafile.close() 
-        core.quit()
-
-    data = '{},{},{},{},{},{},{},{}\n'.format(subjID, age, gender, trialID, letter, x_axis, key, rt) # subjID, age, sexを追加
-    datafile.write(data)
-
-    trialID = trialID + 1
+        else:
+            resp = event.waitKeys(keyList = ['space','escape'], timeStamped = stopwatch)
+            data = '{},{}\n'.format(timeStamped,header[column])
+            key, rt = resp[0]
+            if key == 'escape': 
+                datafile.close() 
+                core.quit()
+            data = '{},{}\n'.format(rt, key)
+            datafile.write(data)
 
 datafile.close()
 win.close()
